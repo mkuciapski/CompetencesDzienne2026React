7abc4c5f63c8026a84c478f26c86cf047283bb6b
Dariusz Kralewski <dariusz.kralewski@ug.edu.pl>
2026-05-20 16:08:31 +0200

    comptence card

--- src/views/CompetencesView.tsx
+++ src/views/CompetencesView.tsx
@@ -1,9 +1,18 @@
-import ButtonCounter from "../components/ButtonCounter";
+import { competences } from "../assets/Competences";
+import { useState } from "react";
+
 export default function Competences() {
+  const [competence] = useState(competences[1]);
   return (
     <div>
-      home
-      <ButtonCounter />
+      <div className="card bg-base-100 w-48 shadow-sm card-spacing">
+        <h2 className="text-center text-xl uppercase">{competence.Name}</h2>
+        <img src={`assets/${competence.Picture}`} />
+        <div className="card-body">
+          <p>Poziom: {competence.Level}</p>
+          <p>Opis: {competence.Description}</p>
+        </div>
+      </div>
     </div>
   );
 }

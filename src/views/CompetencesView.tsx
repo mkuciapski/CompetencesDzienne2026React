b6504643ec95e3040d98f6b1d19e9b6a82d97700
Dariusz Kralewski <dariusz.kralewski@ug.edu.pl>
2026-05-20 16:34:26 +0200

    comptences cards

--- src/views/CompetencesView.tsx
+++ src/views/CompetencesView.tsx
@@ -1,18 +1,18 @@
 import { competences } from "../assets/Competences";
-import { useState } from "react";
 
 export default function Competences() {
-  const [competence] = useState(competences[1]);
-  return (
-    <div>
-      <div className="card bg-base-100 w-48 shadow-sm card-spacing">
-        <h2 className="text-center text-xl uppercase">{competence.Name}</h2>
-        <img src={`assets/${competence.Picture}`} />
-        <div className="card-body">
-          <p>Poziom: {competence.Level}</p>
-          <p>Opis: {competence.Description}</p>
-        </div>
+  // const [competence] = useState(competences[1]);
+  const listCompetences = competences.map((competence) => (
+    <div className="card bg-base-100 w-48 shadow-sm card-spacing">
+      <h2 className="text-center text-xl uppercase">{competence.Name}</h2>
+      <img src={`assets/${competence.Picture}`} />
+      <div className="card-body">
+        <p>Poziom: {competence.Level}</p>
+        <p>Opis: {competence.Description}</p>
       </div>
     </div>
+  ));
+  return (
+    <div className="flex flex-wrap justify-center gap-2">{listCompetences}</div>
   );
 }

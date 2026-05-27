99aa536c7d2703f0f9273f9e9f2e662b025db0a2
mkuciapki <m.kuciapski@gmail.com>
2026-05-27 16:45:34 +0200

    events

--- src/views/CompetencesView.tsx
+++ src/views/CompetencesView.tsx
@@ -1,9 +1,11 @@
 import { competences } from "../assets/Competences";
 import CompetenceCard from "../components/CompetenceCard";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Competences() {
   const [_competences, setCompetences] = useState(competences);
+  const [searchQuery, setSearchQuery] = useState("");
+  const searchText = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     setTimeout(() => {
@@ -19,15 +21,35 @@ export default function Competences() {
       ]);
     }, 5000);
   }, []);
+  const searchCompetence = () => {
+    const query = searchText.current?.value.toLowerCase();
+    setSearchQuery(query);
+  };
+
+  const filteredCompetences = _competences.filter((competence) =>
+    competence.Name.toLowerCase().includes(searchQuery),
+  );
 
   return (
     <div className="flex flex-wrap justify-center gap-2">
-      {_competences.map(
-        (competence, Id) =>
-          competence.Level > 0 && (
-            <CompetenceCard key={competence.Id} competence={competence} />
-          ),
-      )}
+      <button className="btn join-item bg-green-600 text-white border-none cursor-default">
+        Podaj nazwę kompetencji
+      </button>
+      <input
+        type="text"
+        className="input input-bordered join-item w-full max-w-xs"
+        placeholder="..."
+        ref={searchText}
+        onKeyUp={searchCompetence}
+      />
+      <div className="flex flex-wrap justify-center gap-2">
+        {filteredCompetences.map(
+          (competence, Id) =>
+            competence.Level > 0 && (
+              <CompetenceCard key={competence.Id} competence={competence} />
+            ),
+        )}
+      </div>
     </div>
   );
 }
